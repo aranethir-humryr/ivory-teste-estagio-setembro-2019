0c9b47a0f5884b5c8eaadda4ab3c691ea9cbb073
aranethir-humryr <54917796+aranethir-humryr@users.noreply.github.com>
2019-09-04 20:28:35 -0300

    Código da Q1
    Resposta no Readme e código com Loop e chamada recursiva.

---
+++ Q1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace C__Development_and_Studies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int numero = 5;
+            int resultado;
+
+            resultado = Calcular(numero);
+
+            Console.WriteLine("Resultado: {0}",resultado);
+            Console.ReadKey();
+        }
+
+        static int Calcular (int numero){
+            int resultado = new int();
+            if (numero<=0){
+                return 1;
+            }else{
+                //resultado = numero * Calcular(numero-1);
+                for (int cont=numero;cont>0;cont--){
+                    resultado+=numero*numero-1;
+                }
+                return resultado;
+            }
+        }
+    }
+}
